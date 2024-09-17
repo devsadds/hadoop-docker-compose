61e6ba26eb880ab6706ee58c18b9ddac7f2def8b
Галушко Александр Владимирович <avgalu10@mts.ru>
2024-09-17 11:28:32 +0600

    p: upd Вт 17 сен 2024 11:28:32 +06 . Changed files hadoop/cluster/docker-compose.yaml,hadoop/cluster/docker/krb5/build/krb5/starter.sh

--- hadoop/cluster/docker/krb5/build/krb5/starter.sh
+++ hadoop/cluster/docker/krb5/build/krb5/starter.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-if [[ ${SKIP_DB_INIT} == "true" ]];then
-krb_database_name=$(grep database_name ${KRB5_KDC_PROFILE}  | awk -F'=' '{print $2}' |  awk -F'/' '{print $NF}')
+if [[ ${CONTAINER_DEBUG_ON} == "true" ]]; then
 
-if file /var/lib/krb5kdc/${krb_database_name} | grep -qo Berkeley; then 
-    echo "ok. Database ${krb_database_name} exitst IN /var/lib/krb5kdc"
+    if [[ ${SKIP_DB_INIT} == "true" ]];then
+        krb_database_name=$(grep database_name ${KRB5_KDC_PROFILE}  | awk -F'=' '{print $2}' |  awk -F'/' '{print $NF}')
+
+        if file /var/lib/krb5kdc/${krb_database_name} | grep -qo Berkeley; then 
+            echo "ok. Database ${krb_database_name} exitst IN /var/lib/krb5kdc"
+        else
+            echo "Init database ${krb_database_name} IN /var/lib/krb5kdc"
+            cd /var/lib/krb5kdc
+            kdb5_util create -r OMSK.LINUX2BE.COM -P ${KRB5_KDC_PASSWORD} -d ${krb_database_name} -s
+        fi
+    fi
+    echo "Run container in debug mode. Exec tail -f /dev/null"
+    tail -f /dev/null
 else
-    echo "Init database ${krb_database_name} IN /var/lib/krb5kdc"
-    cd /var/lib/krb5kdc
-    kdb5_util create -r OMSK.LINUX2BE.COM -P ${KRB5_KDC_PASSWORD} -d ${krb_database_name} -s
+    echo "Exec $@"
+    "$@"
 fi
-fi
-
-"$@"
