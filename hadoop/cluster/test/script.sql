b780f7a93c0c6827d7aa95e0badb1b1087d1f179
Галушко Александр Владимирович <avgalu10@mts.ru>
2024-09-17 20:32:22 +0600

    p: upd Вт 17 сен 2024 20:32:22 +06 . Changed files hadoop/cluster/test/script.sql

--- hadoop/cluster/test/script.sql
+++ hadoop/cluster/test/script.sql
@@ -1,10 +1,32 @@
 DROP TABLE IF EXISTS my_test_table_4;
-CREATE TABLE IF  not EXISTS  my_test_table_4 (
+
+CREATE TABLE IF NOT EXISTS my_test_table_4 (
   id INT,
   name STRING,
-  desc STRING
+  description STRING,
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  status BOOLEAN
 );
+
 SHOW TABLES;
-INSERT INTO my_test_table_4 (id, name, desc) VALUES (5, 'Alice', 'disc1'),(6, 'Bob', 'disc2');
+
+-- Вставляем 1000 случайных записей
+FROM (
+  SELECT stack(
+    1000,
+    -- Пример генерации случайных значений для каждого поля
+    -- Здесь используется функция rand() для генерации случайных чисел
+    -- и функции from_unixtime() и unix_timestamp() для случайных дат
+    -- Функции concat() и cast() используются для создания строк и приведения типов
+    -- true/false выбираются случайно с помощью rand() > 0.5
+    CAST(rand()*1000 AS INT), CONCAT('Name_', CAST(rand()*1000 AS INT)), CONCAT('Description_', CAST(rand()*1000 AS INT)),
+    from_unixtime(unix_timestamp() - CAST(rand()*10000000 AS INT)),
+    from_unixtime(unix_timestamp() - CAST(rand()*10000000 AS INT)),
+    CASE WHEN rand() > 0.5 THEN true ELSE false END
+  ) AS (id, name, description, created_at, updated_at, status)
+) AS subquery
+INSERT INTO my_test_table_4 (id, name, description, created_at, updated_at, status);
+
 SELECT * FROM my_test_table_4;
-DESCRIBE my_test_table_3;
+DESCRIBE my_test_table_4; -- Исправил на my_test_table_4
