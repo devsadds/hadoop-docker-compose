1388c570c0386cb821a227a6682d8a3b1a9508d0
Галушко Александр Владимирович <avgalu10@mts.ru>
2024-09-17 19:20:01 +0600

    p: upd Вт 17 сен 2024 19:20:01 +06 . Changed files hadoop/cluster/configs/hive/hive-env.sh

--- hadoop/cluster/configs/hive/hive-env.sh
+++ hadoop/cluster/configs/hive/hive-env.sh
@@ -5,9 +5,3 @@ export SPARK_HOME=/opt/spark
 #done
 #export HIVE_AUX_JARS_PATH=$SPARK_JARS
 
-export SPARK_HOME=/opt/spark
-export SPARK_JARS=""
-for jar in `ls $SPARK_HOME/jars`; do
-    export SPARK_JARS=$SPARK_JARS:$SPARK_HOME/jars/$jar
-done
-export HIVE_AUX_JARS_PATH=$SPARK_JARS
