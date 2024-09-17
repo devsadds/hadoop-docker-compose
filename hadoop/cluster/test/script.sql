8097d6c243ae93b0dd372ef582380db80af26955
Галушко Александр Владимирович <avgalu10@mts.ru>
2024-09-17 20:20:18 +0600

    p: upd Вт 17 сен 2024 20:20:18 +06 . Changed files hadoop/cluster/hadoop.cfg,hadoop/cluster/hive.cfg,hadoop/cluster/test/script.sql

--- hadoop/cluster/test/script.sql
+++ hadoop/cluster/test/script.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS my_test_table_3;
-CREATE TABLE IF  not EXISTS  my_test_table_3 (
+DROP TABLE IF EXISTS my_test_table_4;
+CREATE TABLE IF  not EXISTS  my_test_table_4 (
   id INT,
   name STRING,
-  created_at TIMESTAMP
+  desc STRING
 );
 SHOW TABLES;
-INSERT INTO my_test_table_3 (id, name, created_at) VALUES (5, 'Alice', CURRENT_TIMESTAMP),(6, 'Bob', CURRENT_TIMESTAMP);
-SELECT * FROM my_test_table_3;
+INSERT INTO my_test_table_4 (id, name, desc) VALUES (5, 'Alice', 'disc1'),(6, 'Bob', 'disc2');
+SELECT * FROM my_test_table_4;
 DESCRIBE my_test_table_3;
